47d308220b342f9383ec33353105931f3abc86ff
onehae627 <109570580+onehae627@users.noreply.github.com>
2023-07-28 19:57:56 +0900

    모달 만들기

---
+++ index.js
@@ -0,0 +1,11 @@
+const openButton = document.querySelector(".open");
+const container = document.querySelector(".container");
+const closeButton = document.querySelector(".close");
+openButton.addEventListener("click", () => {
+  container.style.display = "flex";
+  openButton.style.display = "none";
+});
+closeButton.addEventListener("click", () => {
+  container.style.display = "none";
+  openButton.style.display = "flex";
+});
