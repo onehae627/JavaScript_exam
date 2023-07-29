97e2fe9852f8e4e70d500dadde6b3a03b7ddd8e0
onehae627 <109570580+onehae627@users.noreply.github.com>
2023-07-29 13:05:44 +0900

    todos 기초

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const openButton = document.querySelector(".open");
-const container = document.querySelector(".container");
-const closeButton = document.querySelector(".close");
-openButton.addEventListener("click", () => {
-  container.style.display = "flex";
-  openButton.style.display = "none";
-});
-closeButton.addEventListener("click", () => {
-  container.style.display = "none";
-  openButton.style.display = "flex";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (input.value !== "") {
+    const li = document.createElement("li");
+    li.innerText = input.value;
+    ul.appendChild(li);
+
+    input.value = "";
+  }
 });
